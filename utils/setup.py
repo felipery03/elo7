b9285755acd449d876a6d2583a984dccd8f0b3ae
Felipe Rigo Yoshimura <feliperigo_yoshimura@hotmail.com>
2022-04-04 09:20:13 -0300

    style - Fix utils setup information

--- utils/setup.py
+++ utils/setup.py
@@ -4,7 +4,7 @@ setup(name='utils',
       version='0.1',
       description='''Utils package to consolidade functions
           used in elo7 case''',
-      url='https://github.com/felipery03/case-elo7/utils-package',
+      url='https://github.com/felipery03/case-elo7/utils',
       author='felipery03',
       author_email='feliperigo_yoshimura@hotmail.com',
       license='MIT',
